71dcc323f8a5ae07b35732aeb71ac80b6294fca2
Semenciuc, Cosmin <cosmin.semenciuc@garmin.com>
2025-08-31 16:02:57 +0300

    Add theme to IOS app

--- composeApp/src/iosMain/kotlin/com/ubb/fmi/orar/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/ubb/fmi/orar/MainViewController.kt
@@ -3,7 +3,12 @@ package com.ubb.fmi.orar
 import androidx.compose.ui.window.ComposeUIViewController
 import com.ubb.fmi.orar.di.KoinInitializer
 import com.ubb.fmi.orar.feature.app.ui.navigation.AppGraph
+import com.ubb.fmi.orar.ui.theme.OrarUbbFmiTheme
 
 fun MainViewController() = ComposeUIViewController(
     configure = { KoinInitializer.initKoin() }
-) { AppGraph() }
+) {
+    OrarUbbFmiTheme {
+        AppGraph()
+    }
+}
