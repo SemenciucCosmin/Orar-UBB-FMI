e98bb45e63eca5cfab6ce29e1d4c2cb97a34547d
Semenciuc, Cosmin <cosmin.semenciuc@garmin.com>
2025-10-15 14:33:22 +0300

    Bump app version from 1.0 to 1.1

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -119,8 +119,8 @@ android {
         applicationId = "com.ubb.fmi.orar"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
     packaging {
         resources {
