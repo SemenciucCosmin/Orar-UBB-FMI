a57e44afcd025e4719a5be90b2909ae3e25c5b2b
Semenciuc, Cosmin <cosmin.semenciuc@garmin.com>
2025-10-27 15:09:45 +0200

    Fix theme selection on IOS

--- composeApp/src/iosMain/kotlin/com/ubb/fmi/orar/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/ubb/fmi/orar/MainViewController.kt
@@ -1,9 +1,15 @@
 package com.ubb.fmi.orar
 
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.window.ComposeUIViewController
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.ubb.fmi.orar.app.AppGraph
 import com.ubb.fmi.orar.di.KoinInitializer
+import com.ubb.fmi.orar.domain.theme.model.ThemeOption
+import com.ubb.fmi.orar.domain.theme.usecase.GetThemeOptionUseCase
 import com.ubb.fmi.orar.ui.theme.OrarUbbFmiTheme
+import org.koin.compose.koinInject
+import kotlin.getValue
 
 /**
  * Main view controller for the Orar UBB FMI application on iOS.
@@ -13,7 +19,12 @@ import com.ubb.fmi.orar.ui.theme.OrarUbbFmiTheme
 fun MainViewController() = ComposeUIViewController(
     configure = { KoinInitializer.initKoin() }
 ) {
-    OrarUbbFmiTheme {
+    val getThemeOptionUseCase: GetThemeOptionUseCase = koinInject()
+    val themeOption by getThemeOptionUseCase().collectAsStateWithLifecycle(
+        initialValue = ThemeOption.SYSTEM
+    )
+
+    OrarUbbFmiTheme(themeOption) {
         AppGraph()
     }
 }
